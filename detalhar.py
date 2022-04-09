d9766a9f3523c32676edcbeda8f9bfeb5a1c7382
Braian d J <71808612+yonk0uz@users.noreply.github.com>
2022-04-09 01:36:33 -0300

    Update detalhar.py

--- detalhar.py
+++ detalhar.py
@@ -2,16 +2,15 @@ from browser import document, alert
 
 def click(event):
     h = document["input_horario"].value
-    resultado = detalhar_horario(h.upper())
-    document["text_dia"].text = resultado['dia']
-    document["text_turno"].text = resultado['turno']
-    document["text_horario"].text = resultado['horario']
-document["btn_horario"].bind("click", click)
+    document["text_resultado"].text = detalhar_horario(h.upper())
+    document["btn_horario"].bind("click", click)
 
 
 def detalhar_horario(h):
+
     ah = []
     try:
+
         for i in h:
             ah.append(i)
 
@@ -20,13 +19,13 @@ def detalhar_horario(h):
             
             if ah[i] == 'T':
                 ah[i] = 'Tarde'
-                turno_pos = i
+
             elif ah[i] == 'M':
                 ah[i] = 'Manhã'
-                turno_pos = i
+
             elif ah[i] == 'N':
                 ah[i] = 'Noite'
-                turno_pos = i            
+            
 
         c = 0
         teste = [[]]
@@ -49,6 +48,8 @@ def detalhar_horario(h):
                 teste[0][i] = 'Quinta'
             elif teste[0][i] == '6':
                 teste[0][i] = 'Sexta'
+            else:
+                exit()
 
         turn = ''.join(teste[1])
         for i in range(len(teste[2])):
@@ -67,20 +68,10 @@ def detalhar_horario(h):
                     teste[2][i] = 'Quinto horario: 17h'
                 elif teste[2][i] == '6':
                     teste[2][i] = 'Sexto horario: 18h'
-            elif turn == 'Noite':
-                if teste[2][i] == '1':
-                    teste[2][i] = 'Primeiro horario: 18h'
-                elif teste[2][i] == '2':
-                    teste[2][i] = 'Segundo horario: 19h'
-                elif teste[2][i] == '3':
-                    teste[2][i] = 'Terceiro horario: 20h'
-                elif teste[2][i] == '4':
-                    teste[2][i] = 'Quarto horario: 21h'
-                elif teste[2][i] == '5':
-                    teste[2][i] = 'Quinto horario: 22h'
-                elif teste[2][i] == '6':
-                    teste[2][i] = 'Sexto horario: 23h'
-            else:
+                else:
+                    exit()
+
+            elif turn =='Manhã':
 
                 if teste[2][i] == '1':
                     teste[2][i] = 'Primeiro horario: 7h'
@@ -94,17 +85,37 @@ def detalhar_horario(h):
                     teste[2][i] = 'Quinto horario: 11h'
                 elif teste[2][i] == '6':
                     teste[2][i] = 'Sexto horario: 12h'
+                else:
+                    exit()
+            elif turn == 'Noite':
 
-        view = f'''Dia: {' - '.join(teste[0])}
-Turno: {''.join(teste[1])}
-Hora: {' - '.join(teste[2])}'''
-        print(view)
+                if teste[2][i] == '1':
+                    teste[2][i] = 'Primeiro horario: 7h'
+                elif teste[2][i] == '2':
+                    teste[2][i] = 'Segundo horario: 8h'
+                elif teste[2][i] == '3':
+                    teste[2][i] = 'Terceiro horario: 9h'
+                elif teste[2][i] == '4':
+                    teste[2][i] = 'Quarto horario: 10h'
+                elif teste[2][i] == '5':
+                    teste[2][i] = 'Quinto horario: 11h'
+                elif teste[2][i] == '6':
+                    teste[2][i] = 'Sexto horario: 12h'
+                exit()
+                
 
-        resultado = {'dia': ' - '.join(teste[0]),
-        'turno': ''.join(teste[1]),
-        'horario': ' - '.join(teste[2])
-        }
-        return resultado
-        return view
+
+        view = f'''\n  
+
+            Dia: [ {' - '.join(teste[0])} ]\n
+             Turno: [ {''.join(teste[1])} ]
+             Hora: [ {' - '.join(teste[2])} ]
+
+                '''
+
+        if "Tarde" in view or "Manhã" in view or "Noite" in view:
+            return view
+        else:
+            return "Catapimbas, horário inexistente"
     except:
-        print("\nDigite o horario corretamente")
+        pass
